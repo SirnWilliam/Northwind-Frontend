f447a0d412127b0d9b8b3133b8411405a9f6af3e
Sirn William <sirn.william@gmail.com>
2019-08-02 00:28:02 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { SERVER_URL } from './components/Constants';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { keyword:'', orderId:'', obfuscatedInfo:'', clearInfo:''}
+  }
+  handleChange = (e) => {
+    this.setState({keyword: e.target.value});
+  }
+  fetchData = () => {
+    fetch(SERVER_URL + `${this.state.keyword}`)
+    .then(response => response.json())
+    .then(responseData => {
+      this.setState({
+        orderId : responseData.orderId,
+      });
+    })
+    .catch(err => console.error(err));
+  }
+  searchData = () => {
+    fetch(SERVER_URL + 'search/' + `${this.state.keyword}`)
+    .then(response => response.json())
+    .then(responseData => {
+      this.setState({
+        obfuscatedInfo : responseData.orderIDInfo.ObfuscatedInfo,
+        clearInfo: responseData.orderIDInfo.ClearInfo,
+      });
+      if(this.state.obfuscatedInfo === null) {
+        toast.error("The ID that you entered is invalid" , {
+          position: toast.POSITION.TOP_CENTER
+          });
+      }
+    })
+    .catch(err => {     
+      console.error(err)
+      })
+  }
+  
+  render() {
+    return(
+      <div className="App">
+        <h3>Min Order ID: 10248</h3>
+        <h3>Max Order ID: 11077</h3>
+        <input type="number" onChange={this.handleChange} placeholder="Enter Order ID" min="10248" max = "11077" style={{color:'blue', width:'150px'}} required/>
+        <button onClick={this.searchData} value={this.state.keyword}>
+          Get ID Information
+        </button>
+        <h3>Obfuscated Information: {this.state.obfuscatedInfo}</h3>
+        <h3>Clear Information: {this.state.clearInfo}</h3>
+        <ToastContainer autoClose={2500}/>
+      </div>
+    );
+  }
 }
 
 export default App;
